f28f0033f25e300e6e5f083001363d04401bf360
Tom McGurl <thomas@chartbeat.com>
2019-01-18 15:28:16 -0500

    Fix RSVP link and Latest build.

--- components/Rsvp.js
+++ components/Rsvp.js
@@ -12,7 +12,7 @@ export default class Rsvp {
           <div class="rsvp-container">
             <iframe id="rsvp-frame" src="https://docs.google.com/forms/d/e/1FAIpQLSfv3BL_5DCgI0Ogpn1xyKPTJSPg8JZl2C6yWgip0ErDmSFcJA/viewform?embedded=true" width="640" height="1266" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
             <div class="rsvp-issues">
-              <h3> If you are having trouble viewing this form please <a href="https://docs.google.com/forms/d/e/1FAIpQLSc1bKv-9iCjc6yqsBASQgDFPjH__7Hcl1Bnf9mJLeTddjgIwg/viewform"> click here </a> </h3>
+              <h3> If you are having trouble viewing this form please <a href="https://docs.google.com/forms/d/e/1FAIpQLSfv3BL_5DCgI0Ogpn1xyKPTJSPg8JZl2C6yWgip0ErDmSFcJA/viewform"> click here </a> </h3>
             </div>
           </div>
         `
