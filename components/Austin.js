383934ffb0a53df9dcde156a05c55ec62027b9b6
Tom McGurl <thomas@chartbeat.com>
2019-01-23 08:05:57 -0500

    Added photos to the Austin page.

--- components/Austin.js
+++ components/Austin.js
@@ -36,32 +36,47 @@ export default class Austin {
                 <div class="line-decoration"></div>
               </div>
 
+              <div class="austin-photo-container austin-photo-left-container">
+                <img src="../images/austin/southCongress.jpg" />
+                <p>
+                  Explore the South Congress district (just a short ride from the Omni!),
+                  where you'll find a host of the weird and wonderful shops, restaurants, and
+                  bars that make Austin so unique.
+                </p>
+              </div>
+              <div class="austin-photo-container austin-photo-right-container">
+                <img src="../images/austin/srv.jpg" />
+                <p>
+                  Check out one of the city's beautiful parks, including Zilker Park, Zilker
+                  Botanical Garden, Ladybird Johnson Wildflower Center, Barton Springs Pool,
+                  or the path around Ladybird Lake (where you can see the famous Stevie Ray
+                  Vaughan statue).
+                </p>
+              </div>
+              <div class="austin-photo-container austin-photo-left-container">
+                <img src="../images/austin/whitehorse.jpg" />
+                <p>
+                  Take a traditional Texas swing or two-step lesson at one of Austin's honky
+                  tonks: the famous <a target="_blank" href="https://www.brokenspokeaustintx.net">Broken Spoke</a> or <a target="_blank" href="https://www.thewhitehorseaustin.com">The White Horse </a>.
+                </p>
+              </div>
+              <div class="austin-photo-container austin-photo-right-container">
+                <img src="../images/austin/lbj.jpg" />
+                <p>
+                  Visit the inspiring <a target="_blank" href="https://blantonmuseum.org">Blanton Museum of Art</a> or
+                  the incredible <a target="_blank" href="http://www.lbjlibrary.org">LBJ Presidential Library and Museum
+                  </a>, both within the University of Texas at Austin's historical campus.
+                </p>
+              </div>
+              <div class="austin-photo-container austin-photo-left-container">
+                <img src="../images/austin/sixthstreet.jpg" />
+                <p>
+                  For some raucous fun, check out the nightlife on 6th Street or Rainey
+                  Street.
+                </p>
+              </div>
               <p>
-                Explore the South Congress district (just a short ride from the Omni!),
-                where you'll find a host of the weird and wonderful shops, restaurants, and
-                bars that make Austin so unique.
-              </p>
-              <p>
-                Check out one of the city's beautiful parks, including Zilker Park, Zilker
-                Botanical Garden, Ladybird Johnson Wildflower Center, Barton Springs Pool,
-                or the path around Ladybird Lake (where you can see the famous Stevie Ray
-                Vaughan statue).
-              </p>
-              <p>
-                Take a traditional Texas swing or two-step lesson at one of Austin's honky
-                tonks: the famous <a target="_blank" href="https://www.brokenspokeaustintx.net">Broken Spoke</a> or <a target="_blank" href="https://www.thewhitehorseaustin.com">The White Horse </a>.
-              </p>
-              <p>
-                Visit the inspiring Blanton Museum of Art <https://blantonmuseum.org/> or
-                the incredible <a target="_blank" href="http://www.lbjlibrary.org">LBJ Presidential Library and Museum
-                </a>, both within the University of Texas at Austin's historical campus.
-              </p>
-              <p>
-                For some raucous fun, check out the nightlife on 6th Street or Rainey
-                Street.
-              </p>
-              <p>
-                Visit Austin's official page <a target="_blank" href="https://www.austintexas.org">https://www.austintexas.org</a> for great
+                Visit Austin's official page <a target="_blank" href="https://www.austintexas.org">austintexas.org</a> for great
                 information on things to see, do, and eat around Texas' capital city.
               </P>
 
@@ -72,31 +87,46 @@ export default class Austin {
                 </div>
                 <div class="line-decoration"></div>
               </div>
-              <p>
-                For authentic Texas BBQ, visit <a target="_blank" href="https://www.terryblacksbbq.com/">Terry Black's BBQ
-                </a> on Barton Springs Road, or wait in line
-                to experience the world-famous <a target="_blank" href="https://franklinbbq.com/">Franklin Barbecue</a>.
-              </p>
-              <p>
-                A trip to Austin is not complete without a visit to <a target="_blank" href="https://torchystacos.com">Torchy's Tacos</a>.
-              </p>
-              <p>
-                Easy Tiger is a great place to grab lunch, where everything from their
-                bread to their pastrami is made in-house. For an awesome burger, try
-                Hopdoddy on South Congress. If you're looking for a place to have a
-                fantastic cocktail and a delicious meal, look no further than <a target="_blank" href="https://oddduckaustin.com">Odd Duck</a>.
-              </p>
-              <p>
-                For coffee, visit <a target="_blank" href="https://www.joscoffee.com">Jo's Coffee</a> on South
-                Congress Ave, <a target="_blank" href="http://www.spokesmancoffee.com">Spokesman Coffee</a>, or <a target="_blank" href="https://www.cosmiccoffeebeer.com">Cosmic
-                Coffee + Beer Garden</a>.
-              </p>
-              <p>
-                Tour one of our many local breweries: <a target="_blank" href="https://jesterkingbrewery.com">Jester King</a>, <a target="_blank" href="http://www.stelmobrewing.com">St. Elmo Brewing Co.<a/>, <a target="_blank" href="https://www.zilkerbeer.com/">Zilker Brewing Co.</a>,
-                <a target="_blank" href="http://www.hopsandgrain.com">Hops & Grain </a>,
-                <a target="_blank" href="https://www.lazarusbrewing.com/">Lazarus Brewing Company</a>, <a target="_blank" href="http://www.skullmechanix.com">Skull Mechanix
-                Brewing </a> (very close to the Omni), <a target="_blank" href="https://theabgb.com">The Austin Beer Garden Brewing Company </a>.
-              </p>
+              <div class="austin-photo-container austin-photo-right-container">
+                <img src="../images/austin/bbq.jpg" />
+                <p>
+                  For authentic Texas BBQ, visit <a target="_blank" href="https://www.terryblacksbbq.com/">Terry Black's BBQ
+                  </a> on Barton Springs Road, or wait in line
+                  to experience the world-famous <a target="_blank" href="https://franklinbbq.com/">Franklin Barbecue</a>.
+                </p>
+              </div>
+              <div class="austin-photo-container austin-photo-left-container">
+                <img src="../images/austin/torchys.png" />
+                <p>
+                  A trip to Austin is not complete without a visit to <a target="_blank" href="https://torchystacos.com">Torchy's Tacos</a>.
+                </p>
+              </div>
+              <div class="austin-photo-container austin-photo-right-container">
+                <img src="../images/austin/oddduck.jpg" />
+                <p>
+                  Easy Tiger is a great place to grab lunch, where everything from their
+                  bread to their pastrami is made in-house. For an awesome burger, try
+                  Hopdoddy on South Congress. If you're looking for a place to have a
+                  fantastic cocktail and a delicious meal, look no further than <a target="_blank" href="https://oddduckaustin.com">Odd Duck</a>.
+                </p>
+              </div>
+              <div class="austin-photo-container austin-photo-left-container">
+                <img src="../images/austin/spokesman.jpg" />
+                <p>
+                  For coffee, visit <a target="_blank" href="https://www.joscoffee.com">Jo's Coffee</a> on South
+                  Congress Ave, <a target="_blank" href="http://www.spokesmancoffee.com">Spokesman Coffee</a>, or <a target="_blank" href="https://www.cosmiccoffeebeer.com">Cosmic
+                  Coffee + Beer Garden</a>.
+                </p>
+              </div>
+              <div class="austin-photo-container austin-photo-right-container">
+                <img src="../images/austin/jesterking2.jpg" />
+                <p>
+                  Tour one of our many local breweries: <a target="_blank" href="https://jesterkingbrewery.com">Jester King</a>, <a target="_blank" href="http://www.stelmobrewing.com">St. Elmo Brewing Co.<a/>, <a target="_blank" href="https://www.zilkerbeer.com/">Zilker Brewing Co.</a>,
+                  <a target="_blank" href="http://www.hopsandgrain.com">Hops & Grain </a>,
+                  <a target="_blank" href="https://www.lazarusbrewing.com/">Lazarus Brewing Company</a>, <a target="_blank" href="http://www.skullmechanix.com">Skull Mechanix
+                  Brewing </a> (very close to the Omni), <a target="_blank" href="https://theabgb.com">The Austin Beer Garden Brewing Company </a>.
+                </p>
+              </div>
 
               <div class="header-with-decoration">
                 <div class="line-decoration"></div>
@@ -105,21 +135,26 @@ export default class Austin {
                 </div>
                 <div class="line-decoration"></div>
               </div>
-              <p>
-                <a target="_blank" href="http://continentalclub.com/">The Continental Club</a> has been an Austin
-                institution since the '50s. Located on bustling South Congress Avenue, the
-                club has live music nearly every night of the week, ranging from blues to
-                rock to traditional country.
-              </p>
-              <p>
-                <a target="_blank" href="http://www.cboys.com/">C-Boy's Heart & Soul</a> is another great club on South
-                Congress where you can catch live music or try your luck at chicken-shit
-                bingo.
-              </p>
-              <p>
-                <a target="_blank" href="https://www.elephantroom.com">The Elephant Room </a> is a hole-in-the-wall
-                jazz club with live music nightly.
-              </p>
+              <div class="austin-photo-container austin-photo-left-container">
+                <img src="../images/austin/continentalclub.jpg" />
+                <p>
+                  <a target="_blank" href="http://continentalclub.com/">The Continental Club</a> has been an Austin
+                  institution since the '50s. Located on bustling South Congress Avenue, the
+                  club has live music nearly every night of the week, ranging from blues to
+                  rock to traditional country.
+                </p>
+              </div>
+              <div class="austin-photo-container austin-photo-right-container">
+                <p>
+                  <a target="_blank" href="http://www.cboys.com/">C-Boy's Heart & Soul</a> is another great club on South
+                  Congress where you can catch live music or try your luck at chicken-shit
+                  bingo.
+                </p>
+                <p>
+                  <a target="_blank" href="https://www.elephantroom.com">The Elephant Room </a> is a hole-in-the-wall
+                  jazz club with live music nightly.
+                </p>
+              </div>
             </div>
           </div>
         `
